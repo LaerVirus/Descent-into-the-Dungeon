c56dccd7f3f52841d20c0fbb96aaa62cd79ad0f8
Aliaksei Veras <aliaksei.veras@gmail.com>
2021-04-11 17:14:23 +0300

    Map-

--- Descent-into-the-Dungeon/Form1.Designer.cs
+++ Descent-into-the-Dungeon/Form1.Designer.cs
@@ -29,13 +29,163 @@ namespace Descent_into_the_Dungeon
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Map = new System.Windows.Forms.Panel();
+            this.enemy7 = new System.Windows.Forms.PictureBox();
+            this.enemy5 = new System.Windows.Forms.PictureBox();
+            this.enemy6 = new System.Windows.Forms.PictureBox();
+            this.enemy3 = new System.Windows.Forms.PictureBox();
+            this.enemy4 = new System.Windows.Forms.PictureBox();
+            this.enemy2 = new System.Windows.Forms.PictureBox();
+            this.enemy1 = new System.Windows.Forms.PictureBox();
+            this.mainHero = new System.Windows.Forms.PictureBox();
+            this.Map.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainHero)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Map
+            // 
+            this.Map.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Map.Controls.Add(this.enemy7);
+            this.Map.Controls.Add(this.enemy5);
+            this.Map.Controls.Add(this.enemy6);
+            this.Map.Controls.Add(this.enemy3);
+            this.Map.Controls.Add(this.enemy4);
+            this.Map.Controls.Add(this.enemy2);
+            this.Map.Controls.Add(this.enemy1);
+            this.Map.Controls.Add(this.mainHero);
+            this.Map.Location = new System.Drawing.Point(227, 147);
+            this.Map.Name = "Map";
+            this.Map.Size = new System.Drawing.Size(400, 100);
+            this.Map.TabIndex = 0;
+            // 
+            // enemy7
+            // 
+            this.enemy7.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.enemy7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("enemy7.BackgroundImage")));
+            this.enemy7.Location = new System.Drawing.Point(710, 10);
+            this.enemy7.Name = "enemy7";
+            this.enemy7.Size = new System.Drawing.Size(80, 80);
+            this.enemy7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy7.TabIndex = 3;
+            this.enemy7.TabStop = false;
+            // 
+            // enemy5
+            // 
+            this.enemy5.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.enemy5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("enemy5.BackgroundImage")));
+            this.enemy5.Location = new System.Drawing.Point(510, 10);
+            this.enemy5.Name = "enemy5";
+            this.enemy5.Size = new System.Drawing.Size(80, 80);
+            this.enemy5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy5.TabIndex = 3;
+            this.enemy5.TabStop = false;
+            // 
+            // enemy6
+            // 
+            this.enemy6.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.enemy6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("enemy6.BackgroundImage")));
+            this.enemy6.Location = new System.Drawing.Point(610, 10);
+            this.enemy6.Name = "enemy6";
+            this.enemy6.Size = new System.Drawing.Size(80, 80);
+            this.enemy6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy6.TabIndex = 2;
+            this.enemy6.TabStop = false;
+            // 
+            // enemy3
+            // 
+            this.enemy3.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.enemy3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("enemy3.BackgroundImage")));
+            this.enemy3.Location = new System.Drawing.Point(310, 10);
+            this.enemy3.Name = "enemy3";
+            this.enemy3.Size = new System.Drawing.Size(80, 80);
+            this.enemy3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy3.TabIndex = 3;
+            this.enemy3.TabStop = false;
+            // 
+            // enemy4
+            // 
+            this.enemy4.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.enemy4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("enemy4.BackgroundImage")));
+            this.enemy4.Location = new System.Drawing.Point(410, 10);
+            this.enemy4.Name = "enemy4";
+            this.enemy4.Size = new System.Drawing.Size(80, 80);
+            this.enemy4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy4.TabIndex = 2;
+            this.enemy4.TabStop = false;
+            // 
+            // enemy2
+            // 
+            this.enemy2.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.enemy2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("enemy2.BackgroundImage")));
+            this.enemy2.Location = new System.Drawing.Point(210, 10);
+            this.enemy2.Name = "enemy2";
+            this.enemy2.Size = new System.Drawing.Size(80, 80);
+            this.enemy2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy2.TabIndex = 2;
+            this.enemy2.TabStop = false;
+            // 
+            // enemy1
+            // 
+            this.enemy1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.enemy1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("enemy1.BackgroundImage")));
+            this.enemy1.Location = new System.Drawing.Point(110, 10);
+            this.enemy1.Name = "enemy1";
+            this.enemy1.Size = new System.Drawing.Size(80, 80);
+            this.enemy1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy1.TabIndex = 1;
+            this.enemy1.TabStop = false;
+            // 
+            // mainHero
+            // 
+            this.mainHero.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.mainHero.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("mainHero.BackgroundImage")));
+            this.mainHero.Location = new System.Drawing.Point(10, 10);
+            this.mainHero.Name = "mainHero";
+            this.mainHero.Size = new System.Drawing.Size(80, 80);
+            this.mainHero.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mainHero.TabIndex = 0;
+            this.mainHero.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(874, 437);
+            this.Controls.Add(this.Map);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Map.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.enemy7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainHero)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel Map;
+        private System.Windows.Forms.PictureBox mainHero;
+        private System.Windows.Forms.PictureBox enemy1;
+        private System.Windows.Forms.PictureBox enemy7;
+        private System.Windows.Forms.PictureBox enemy5;
+        private System.Windows.Forms.PictureBox enemy6;
+        private System.Windows.Forms.PictureBox enemy3;
+        private System.Windows.Forms.PictureBox enemy4;
+        private System.Windows.Forms.PictureBox enemy2;
     }
 }
 
--- Descent-into-the-Dungeon/Form1.cs
+++ Descent-into-the-Dungeon/Form1.cs
@@ -12,9 +12,52 @@ namespace Descent_into_the_Dungeon
 {
     public partial class Form1 : Form
     {
+        Point leftBorder;
+        Point rightBorder;
+
         public Form1()
         {
             InitializeComponent();
+            leftBorder = mainHero.Location;
+            rightBorder = enemy3.Location;
+            Map.MouseWheel += new MouseEventHandler(Map_MouseWheel);
+        }
+
+        private void Map_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (e.Delta > 0 && mainHero.Location != leftBorder)
+            {
+                System.Threading.TimerCallback tm = new System.Threading.TimerCallback(MoveMap);
+                Timer timer = new Timer(MoveMap, 1, 0, 1000);
+                MoveMap(10);
+            }
+            if (e.Delta < 0 && enemy7.Location != rightBorder)
+            {
+                MoveMap(-10);
+            }
+        }
+
+        private void Map_MouseEnter(object sender, EventArgs e)
+        {
+            mainHero.MouseWheel += Map_MouseWheel;
+        }
+
+        private void Map_MouseLeave(object sender, EventArgs e)
+        {
+            Map.MouseWheel -= Map_MouseWheel;
+        }
+
+        private void MoveMap(object distance)
+        {
+            int run = Convert.ToInt32(distance);
+            mainHero.Location = new Point(mainHero.Location.X + run, mainHero.Location.Y);
+            enemy1.Location = new Point(enemy1.Location.X + run, enemy1.Location.Y);
+            enemy2.Location = new Point(enemy2.Location.X + run, enemy2.Location.Y);
+            enemy3.Location = new Point(enemy3.Location.X + run, enemy3.Location.Y);
+            enemy4.Location = new Point(enemy4.Location.X + run, enemy4.Location.Y);
+            enemy5.Location = new Point(enemy5.Location.X + run, enemy5.Location.Y);
+            enemy6.Location = new Point(enemy6.Location.X + run, enemy6.Location.Y);
+            enemy7.Location = new Point(enemy7.Location.X + run, enemy7.Location.Y);
         }
     }
 }
